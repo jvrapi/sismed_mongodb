1d519e19467e68825d6160f94004c0912a930bc2
João Vitor <joaooviitorr@hotmail.com>
2020-01-22 09:37:11 -0300

    Criação do projeto

---
+++ src/main/java/br/com/sismed/mongodb/SismedMongodbApplication.java
@@ -0,0 +1,13 @@
+package br.com.sismed.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SismedMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SismedMongodbApplication.class, args);
+	}
+
+}
